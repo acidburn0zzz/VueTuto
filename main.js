18df0d5b2ead6ef82900689e5b1eec83a2ba4400
Stéphane Bullier <manix.thedevelopper@gmail.com>
2016-04-13 14:36:44 +0200

    Rendering and working with lists : step 1

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by stephane on 13/04/16.
+ */
+new Vue({
+    el: '#app',
+
+    data: {
+        tasks: [
+            { body: 'Go to the shop', completed: false },
+            { body: 'Go to the bank', completed: false },
+            { body: 'Go to the dentist', completed: true }
+        ]
+    }
+})
