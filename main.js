28714d5185fcf1e8f0837e9f42d247b58786e22a
Stéphane Bullier <manix.thedevelopper@gmail.com>
2016-04-16 17:57:59 +0200

    Example de choix multiple dans une liste déroulante

--- main.js
+++ main.js
@@ -1,43 +1,11 @@
 var vm = new Vue({
   el: '#app',
   data: {
-    personnes: [
-      {nom: "Claret", prenom: "Marcel"},
-      {nom: "Durand", prenom: "Jacques"},
-      {nom: "Dupont", prenom: "Albert"},
-      {nom: "Martin", prenom: "Denis"},
-      {nom: "Torlet", prenom: "Arthur"},
-    ],
-    trash: []
-  },
-  methods: {
-    erase (index) {
-      this.trash.push(this.personnes[index]);
-      this.personnes.splice(index,1);
-      this.trash.sort(order);
-    },
-    restore (index) {
-      this.personnes.push(this.trash[index]);
-      this.trash.splice(index, 1);
-      this.personnes.sort(order);
-    },
-    Delete (index) {
-      this.trash.splice(index, 1)
-    },
-    eraseAll () {
-      this.trash = this.trash.concat(this.personnes);
-      this.trash.sort(order);
-      this.personnes = [];
-    },
-    restoreAll () {
-      this.personnes = this.personnes.concat(this.trash);
-      this.personnes.sort(order);
-      this.trash = [];
-    },
-    deleteAll () {
-      this.trash = [];
-    }
+    choix: [1, 3],
+    options: [
+      { text: 'Choix 1', value: '1' },
+      { text: 'Choix 2', value: '2' },
+      { text: 'Choix 3', value: '3' }
+    ]
   }
 });
-
-var order = function (a, b) { return (a.nom > b.nom) };
