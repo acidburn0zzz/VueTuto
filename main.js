1f106da20277dc4fdbb2c1e85a11eb0ca7defbec
Stéphane Bullier <manix.thedevelopper@gmail.com>
2016-04-16 17:46:39 +0200

    Gestion d'utilisateurs

--- main.js
+++ main.js
@@ -2,12 +2,42 @@ var vm = new Vue({
   el: '#app',
   data: {
     personnes: [
+      {nom: "Claret", prenom: "Marcel"},
       {nom: "Durand", prenom: "Jacques"},
       {nom: "Dupont", prenom: "Albert"},
       {nom: "Martin", prenom: "Denis"},
-    ]
+      {nom: "Torlet", prenom: "Arthur"},
+    ],
+    trash: []
+  },
+  methods: {
+    erase (index) {
+      this.trash.push(this.personnes[index]);
+      this.personnes.splice(index,1);
+      this.trash.sort(order);
+    },
+    restore (index) {
+      this.personnes.push(this.trash[index]);
+      this.trash.splice(index, 1);
+      this.personnes.sort(order);
+    },
+    Delete (index) {
+      this.trash.splice(index, 1)
+    },
+    eraseAll () {
+      this.trash = this.trash.concat(this.personnes);
+      this.trash.sort(order);
+      this.personnes = [];
+    },
+    restoreAll () {
+      this.personnes = this.personnes.concat(this.trash);
+      this.personnes.sort(order);
+      this.trash = [];
+    },
+    deleteAll () {
+      this.trash = [];
+    }
   }
 });
 
-lg = vm.personnes.length;
-vm.personnes.$set(1, {nom: "Claret", prenom: "Marcel"});
+var order = function (a, b) { return (a.nom > b.nom) };
